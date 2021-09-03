19d796c70b66db509542f130620197aad59a0305
Jesinug <jesica.nugnez@gmail.com>
2021-09-03 23:48:36 +0200

    MVP working! Github API consumed and card app rendering

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import CardList from './Components/CardList/CardList';
 import Form from './Components/Form/Form';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-        profiles: [],
-      }; 
-    
-  let addNewProfile = (profileData) => {
-    this.setState(prevState => ({
-      profiles: [...prevState.profiles, profileData],
-    }))
-  }
-}
+  state = {
+    profiles: [],
+  };
+   addNewProfile = (profileData) => {
+  	this.setState(prevState => ({
+    	profiles: [...prevState.profiles, profileData],
+    }));
+  };
   render() {
     return (
       <>
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,21 +3,22 @@ import React from 'react';
 import './Form.css'
 
 class Form extends React.Component {
-    state = { userName: ''}
+    state = { username: ''}
     handleSubmit = async (event) => { 
       event.preventDefault();
       const resp = await axios.get(`https://api.github.com/users/${this.state.username}`);
       this.props.onSubmit(resp.data);
       this.setState({ username: '' });
     };
+
       render() {
         return (
           <form onSubmit={ this.handleSubmit }>
             <input
               type='text'
-              placeholder='Github username'
-              value = {this.state.userName}
-              onChange = { event => this.setState({ userName: event.target.value })}
+              placeholder='Type Github username'
+              value = {this.state.username}
+              onChange = { event => this.setState({ username: event.target.value })}
               required
               />
             <button>Add Card</button>
